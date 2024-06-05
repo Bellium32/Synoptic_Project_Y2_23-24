8c5d4b23f5cfcbb8f0510dc4b3d45db2f68364c3
Blackxlisted <136181049+Blackxlisted@users.noreply.github.com>
2024-06-05 23:25:10 +0100

    Create alert.py

---
+++ alert.py
@@ -0,0 +1,20 @@
+import smtplib
+from email.message import EmailMessage
+
+def email_alert(subject,body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+
+    user = "ashishjoseph552@gmail.com"
+    password = "Shaji2alphonsa"
+
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login(user, password)
+
+    server.quit()
+
+    if __name__ == '__main__':
+        email_alert("hey", "ash", "ashishJsoeph553@gmail.com")
