86ab1cb549cee241e6fa8652cd5664abe50bad10
Bellium32 <116981843+Bellium32@users.noreply.github.com>
2024-06-07 14:52:24 +0100

    Update weatherDB.py

--- weatherDB.py
+++ weatherDB.py
@@ -1,5 +1,5 @@
 import mysql.connector
-#Guide to establishing the connection
+################Guide to establishing the connection##########################
 #Have SQL Workbench, Installer, and Shell instealled on the device
 #Have python and it's dependencies installed, if using VS studio install "MySQL Shell for VS Code"
 #To establish the connection, press "Windows key + R", them type in "cmd"
@@ -37,9 +37,9 @@ else:
 def my_Tables_Create():
     #SQL commands to create tables
     mycursor.execute("CREATE TABLE weatherDay (id INT AUTO_INCREMENT PRIMARY KEY, location VARCHAR(5), day VARCHAR(10), date VARCHAR(15),"
-                     "meanTemp VARCHAR(4), minTemp VARCHAR(4), maxTemp VARCHAR(4), conditions VARCHAR(63))")
+                     "meanTemp VARCHAR(7), minTemp VARCHAR(7), maxTemp VARCHAR(7), conditions VARCHAR(63))")
     mycursor.execute("CREATE TABLE weatherHour (id INT AUTO_INCREMENT PRIMARY KEY, location VARCHAR(5), hour VARCHAR(7), date VARCHAR(15),"
-                    "meanTemp VARCHAR(4), minTemp VARCHAR(4), maxTemp VARCHAR(4), conditions VARCHAR(63))")
+                    "meanTemp VARCHAR(7), minTemp VARCHAR(7), maxTemp VARCHAR(7), conditions VARCHAR(63))")
     mycursor.execute("CREATE TABLE people (id INT AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(255), lname VARCHAR(255), phoneNumber VARCHAR(15))")
 
 def my_Tables_Drop():
@@ -48,16 +48,16 @@ def my_Tables_Drop():
     mycursor.execute("DROP TABLE IF EXISTS weatherHour")
     mycursor.execute("DROP TABLE IF EXISTS people")
    
-createAndDrop = 8
+createAndDrop = 0
 if createAndDrop == 1:
     print("Free space")
 if createAndDrop == 2:
-    my_Tables_Create
+    my_Tables_Create()
 if createAndDrop == 98:
     mycursor.execute("DROP DATABASE weatherdb")
 
 if createAndDrop == 99:
-    my_Tables_Drop
+    my_Tables_Drop()
     
 def my_Day_Insert(location, day, date, meanTemp, minTemp, maxTemp, conditions):
     #Inserts data into the database, just pass the arguments into the commands 
